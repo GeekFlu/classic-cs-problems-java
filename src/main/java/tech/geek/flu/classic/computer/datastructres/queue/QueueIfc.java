b38634064eb2cfbbfcb1e936b31f362d72ce5de1
Spike Storm <software.j2ee@gmail.com>
2020-12-13 21:18:50 -0800

    Queue implementation using an Array of generic types

---
+++ src/main/java/tech/geek/flu/classic/computer/datastructres/queue/QueueIfc.java
@@ -0,0 +1,9 @@
+package tech.geek.flu.classic.computer.datastructres.queue;
+
+public interface QueueIfc<T> {
+  T dequeue();
+  boolean enqueue(T value);
+  int size();
+  T front();
+  boolean isEmpty();
+}
---
+++ src/test/java/tech/geek/flu/classic/computer/problems/queue/QueueArrayTest.java
@@ -0,0 +1,41 @@
+package tech.geek.flu.classic.computer.problems.queue;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tech.geek.flu.classic.computer.datastructres.queue.QueueArray;
+import tech.geek.flu.classic.computer.problems.TestUtils;
+
+import java.util.List;
+
+@Slf4j
+class QueueArrayTest {
+
+  @Test
+  void test_queue_creation() {
+    QueueArray<String> queueArray = new QueueArray<>(String.class);
+    Assertions.assertEquals(0, queueArray.size());
+    Assertions.assertTrue(queueArray.isEmpty());
+  }
+
+  @Test
+  void test_enqueue() {
+    QueueArray<String> queueArray = new QueueArray<>(String.class);
+    queueArray.enqueue("Persona 1");
+    queueArray.enqueue("Persona 2");
+    Assertions.assertEquals("Persona 1", queueArray.front());
+  }
+
+  @Test
+  void test_increase_capacity() {
+    QueueArray<Integer> queueArray = new QueueArray<>(Integer.class);
+    TestUtils.getSampleData(250)
+        .forEach(queueArray::enqueue);
+    Assertions.assertEquals(250, queueArray.size());
+    while (!queueArray.isEmpty()) {
+      queueArray.dequeue();
+    }
+    Assertions.assertEquals(0, queueArray.size());
+  }
+
+}
