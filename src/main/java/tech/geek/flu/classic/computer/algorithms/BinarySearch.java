f19aae1a9a59c0196480cea60a44440c77ad0e4e
Spike Storm <software.j2ee@gmail.com>
2021-01-14 18:14:23 -0800

    Binary Search Algorithm

---
+++ src/main/java/tech/geek/flu/classic/computer/algorithms/BinarySearch.java
@@ -0,0 +1,16 @@
+package tech.geek.flu.classic.computer.algorithms;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Setter
+@Slf4j
+public class BinarySearch {
+
+  public static int binarySearch(int[] arr, int target) {
+    return 0;
+  }
+
+}
